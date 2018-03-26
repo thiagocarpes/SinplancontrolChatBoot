8ce19b2f861c55951ca9ad7137668f9933fce071
Thiago Carpes <thiagocarpes@gmail.com>
2018-03-26 11:27:09 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/sinplancontrolchatboot/MainActivity.java
@@ -0,0 +1,19 @@
+package br.usjt.sinplancontrolchatboot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void enviaNome(View view){
+        Intent intent = new Intent(this, Chatboot.class);
+    }
+}
